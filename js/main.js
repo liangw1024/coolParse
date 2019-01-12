7e9076220733b138fd1a2417c04d813e1b3a098a
liangwei <weibao1024@gmail.com>
2019-01-12 22:16:46 +0800

    format code

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+layui.use(['layer', 'form','element'], function(){
+  var layer = layui.layer
+  ,form = layui.form
+  ,element = layui.element;
+});
+window.onload= onload;
+var onload = function(){
+	var footer = document.querySelector(".footer");
+};
+
